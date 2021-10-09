0d64073750b9760ac4258cd0f0c496f2f78a007b
mlescure <mlescure@mlesucre.fr>
2021-10-09 17:24:41 +0200

    s

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapps.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -108,6 +108,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 django_heroku.settings(locals())
