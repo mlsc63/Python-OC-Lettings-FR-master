2c4dbeffe4c12a7a519c65cb969b86d5c5103e96
mlescure <mlescure@mlesucre.fr>
2021-10-15 10:51:01 +0200

    refactor urls.py for flake

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from . import views
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
 
 urlpatterns = [
-    #path('sentry-debug/', trigger_error),
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('', include('letting.urls')),
