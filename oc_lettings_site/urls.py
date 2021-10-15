72df2e33cd2b6669e84f5eb55d2302aa7057e16d
mlescure <mlescure@mlesucre.fr>
2021-10-15 12:01:16 +0200

    refactor app oc_lettings_site

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
-
+from oc_lettings_site import views
 
 urlpatterns = [
     path('', views.index, name='index'),
