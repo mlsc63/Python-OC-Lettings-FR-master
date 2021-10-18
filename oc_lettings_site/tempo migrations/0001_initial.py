e1725fc4e4d348890648126ca27bdf17b54909f2
mlescure <mlescure@mlesucre.fr>
2021-10-18 10:46:06 +0200

    flake8

--- oc_lettings_site/tempo migrations/0001_initial.py
+++ oc_lettings_site/tempo migrations/0001_initial.py
@@ -24,8 +24,8 @@ class Migration(migrations.Migration):
                     validators=[django.core.validators.MaxValueValidator(9999)])),
                 ('street', models.CharField(max_length=64)),
                 ('city', models.CharField(max_length=64)),
-                ('state', models.CharField(max_length=2,
-                                           validators=[django.core.validators.MinLengthValidator(2)])),
+                ('state', models.CharField(
+                    max_length=2, validators=[django.core.validators.MinLengthValidator(2)])),
                 ('zip_code', models.PositiveIntegerField(
                     validators=[django.core.validators.MaxValueValidator(99999)])),
                 ('country_iso_code', models.CharField(
