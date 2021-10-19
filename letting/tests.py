eec255c31a457e7e1658b74d4aaba90659054528
mlescure <mlescure@mlesucre.fr>
2021-10-19 13:07:15 +0200

    pep8

--- letting/tests.py
+++ letting/tests.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import User
 from app.models import Address
 from django.urls import reverse
 
-class TestLeting(TestCase):
+
+class TestLetting(TestCase):
     def setUp(self):
-        self.address = Address.objects.create(number=105, street="boulevard desaix", city="riom", state="test", zip_code=63200, country_iso_code="")
+        self.address = Address.objects.create(number=105, street="boulevard desaix", city="riom", state="test",
+                                              zip_code=63200, country_iso_code="")
         self.letting = Letting.objects.create(title="test titre", address=self.address)
         self.reverse = reverse('lettings:lettings_index')
-
 
     def test_animals_can_speak(self):
         response = self.client.get(self.reverse)
